910307233ee87182abeac6af6790b960893b9635
boostless <46201063+boostless@users.noreply.github.com>
2019-01-04 21:39:32 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,6 @@ module.exports.run = async (bot, message, args)=>{
   .setDescription("~Pagalba~")
   .setColor("#4f80ff")
   .addField("Komandos", "$report @vardas kodel \n $serverinfo \n $botinfo \n $kick @vardas kodel \n $ban @vardas kodel \n $tempmute @vardas s/m/h/d \n $clear kiekis \n $level \n $ping \n $coins \n $send @vardas kiekis \n $prefix \n $buy /vip/dj/member+");
-  message.delete().catch(O_o=>{});
   message.author.send(helpembed);
 }
 
